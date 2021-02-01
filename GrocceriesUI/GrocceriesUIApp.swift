016c58ffbb1f8f02f14c32ec6cfa22b9aaee9b8a
rohitsainier <rohitsainier@gmail.com>
2021-02-01 21:46:13 +0530

    Initial Commit

---
+++ GrocceriesUI/GrocceriesUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrocceriesUIApp.swift
+//  GrocceriesUI
+//
+//  Created by Rohit Saini on 01/02/21.
+//
+
+import SwiftUI
+
+@main
+struct GrocceriesUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
